03853182f34bc648e7fda9bfa78a2965c537988b
Mauro Lepore <maurolepore@gmail.com>
2019-02-18 13:25:59 -0500

    Clean

--- R/gh_ls.R
+++ R/gh_ls.R
@@ -1,60 +1,18 @@
-gh_branches <- function(path) {
-  owner_repo <- owner_repo(path)
-  purrr::map_chr(gh::gh(glue::glue("/repos/{owner_repo}/branches")), "name")
-}
-
-
 #' Convert a path such as owner/repo/subdir into an `endpoint` for `gh::gh()`.
 #'
-#' @param path
+#' @inheritParams gh_get
 #'
 #' @return A character string.
-#' @export
 #'
 #' @examples
 #' gh_path("r-lib")
 #' gh_path("r-lib/gh")
 #' gh_path("r-lib/gh/tests")
 #' gh_path("r-lib/gh/tests/testthat")
+#' @noRd
+#' @keywords internal
 gh_path <- function(path) {
   piece_apply(path, request_owner, request_repo, request_subdir)
-}
-
-#' List files on GitHub and their html URLs.
-#'
-#' @param path A string formatted as "owner/repo/subdir_1/subdir_2/subdir_n".
-#'
-#' @return
-#'   * `gh_ls()` returns a character vector giving the in a GitHub directory.
-#'   * `gh_html()` returns the html URLs of the files in a GitHub directory.
-#' @export
-#'
-#' @examples
-#' gh_ls("r-lib")
-#' gh_ls("r-lib/gh")
-#' gh_ls("r-lib/gh/tests")
-#' gh_ls("r-lib/gh/tests/testthat")
-#'
-#' gh_html("r-lib")
-#' gh_html("r-lib/gh")
-#' gh_html("r-lib/gh/tests")
-#' gh_html("r-lib/gh/tests/testthat")
-gh_ls <- function(path) {
-  purrr::map_chr(gh::gh(gh_path(path)), "name")
-}
-
-gh_html <- function(path) {
-  purrr::map_chr(gh::gh(gh_path(path)), "html_url")
-}
-
-piece_apply <- function(path, f1, f2, f3) {
-  n_pieces <- as.character(length(split_url(path)))
-  switch(
-    n_pieces,
-    "1" = f1(path),
-    "2" = f2(path),
-    f3(path)
-  )
 }
 
 request_owner <- function(path) {
@@ -97,6 +55,79 @@ subdir <- function(x) {
 
 
 
+
+
+
+#' Get a response from the GitHub API.
+#'
+#' @param path A string formatted as "owner/repo/subdir_1/subdir_2/subdir_n".
+#' @param ref Branch name.
+#'
+#' @family functions to get github responses
+#' @seealso ghr_fields
+#'
+#' @return A list.
+#' @export
+#'
+#' @examples
+#' gh_get("r-lib/usethis")
+#' ghr_path(gh_get("r-lib/usethis", ref = "gh-pages"))
+#' # Same
+#' ghr_path(gh_get("r-lib/usethis@gh-pages"))
+#' ghr_path(gh_get("r-lib/usethis/news@gh-pages"))
+gh_get <- function(path, ref = "master") {
+  gh_get_ <- function() {
+    branch <- ref
+    pieces <- strsplit(path, "@")[[1]]
+    if (length(pieces) > 1) {
+      path <- pieces[[1]]
+      branch <- pieces[[2]]
+    }
+    gh::gh(gh_path(path), ref = branch)
+  }
+  memoise::memoise(gh_get_)
+}
+
+# gh_get_ <- function(path, ref = "master") {
+#   branch <- ref
+#   pieces <- strsplit(path, "@")[[1]]
+#   if (length(pieces) > 1) {
+#     path <- pieces[[1]]
+#     branch <- pieces[[2]]
+#   }
+#
+#   gh::gh(gh_path(path), ref = branch)
+# }
+# gh_get <- memoise::memoise(gh_get_)
+
+
+#' Get the name of all branches of a GitHub repository.
+#'
+#' @inheritParams gh_get
+#'
+#' @family functions to get github responses
+#' @seealso ghr_fields
+#'
+#' @return A character string.
+#' @export
+#'
+#' @examples
+#' gh_branches("r-lib/usethis")
+gh_branches <- function(path) {
+  owner_repo <- owner_repo(path)
+  purrr::map_chr(gh::gh(glue::glue("/repos/{owner_repo}/branches")), "name")
+}
+
+#' Show and pull available fields of a GitHub response.
+#'
+#' @param gh_response A list object of class "gh_response".
+#'
+#' @family functions to work with github responses
+#' @seealso gh_get gh_branches
+#'
+#' @return A character string.
+#' @export
+#'
 #' @examples
 #' gh_response <- gh_get("hadley/babynames/data-raw")
 #' ghr_fields(gh_response)
@@ -112,18 +143,6 @@ subdir <- function(x) {
 #' # Same
 #' ghr_path(gh_get("r-lib/usethis@gh-pages"))
 #' ghr_path(gh_get("r-lib/usethis/news@gh-pages"))
-gh_get_ <- function(path, ref = "master") {
-  branch <- ref
-  pieces <- strsplit(path, "@")[[1]]
-  if (length(pieces) > 1) {
-    path <- pieces[[1]]
-    branch <- pieces[[2]]
-  }
-
-  gh::gh(gh_path(path), ref = branch)
-}
-gh_get <- memoise::memoise(gh_get_)
-
 ghr_fields <- function(gh_response) {
   stopifnot(inherits(gh_response, "gh_response"))
   names(gh_response[[1]])
@@ -141,10 +160,3 @@ with_field <- function(field) {
 ghr_path <- with_field("path")
 ghr_html_url <- with_field("html_url")
 ghr_download_url <- with_field("download_url")
-
-
-
-gh_branches <- function(path) {
-  path_ <- gh_path(path)
-  gh::gh(glue::glue("{path_}/branches"))
-}
