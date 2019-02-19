5184e5b4fff0afd6d1ba795e88384ccb80793aaf
Mauro Lepore <maurolepore@gmail.com>
2019-02-19 13:15:25 -0500

    Revert "Add class and attribute to ghr_ls"
    This reverts commit fad1dcef7d5160b3b1cb31b2f155772adce4814b.

--- tests/testthat/test-ghr_ls.R
+++ tests/testthat/test-ghr_ls.R
@@ -6,10 +6,7 @@ test_that("ghr_ls is sensitive to all arguments", {
   skip_if_net_down()
 
   expect_true("DESCRIPTION" %in% ghr_ls("maurolepore/ghr"))
-
-  out <- unclass(ghr_ls("maurolepore/ghr/R", regexp = "ghr_ls"))
-  attr(out, "path") <- NULL
-  expect_equal(out, "R/ghr_ls.R")
+  expect_equal(ghr_ls("maurolepore/ghr/R", regexp = "ghr_ls"), "R/ghr_ls.R")
 
   expect_true("R/ghr_ls.R" %in% ghr_ls("maurolepore/ghr/R", regexp = "ghr_ls"))
   expect_false(
@@ -22,19 +19,4 @@ test_that("ghr_ls is sensitive to all arguments", {
   expect_true(
     "R/ghr_ls.R" %in% ghr_ls("maurolepore/ghr/R", "GHR_LS", ignore.case = TRUE)
   )
-})
-
-test_that("ghr_ls outputs the expected class", {
-  skip_if_net_down()
-
-  expect_is(ghr_ls("maurolepore/tor"), "ghr_path_ls")
-  expect_equal(
-    class(new_ghr_path_ls(ghr_ls("maurolepore/tor"))),
-    c("ghr_path_ls", "character")
-  )
-})
-
-test_that("ghr_ls has attribute path", {
-  path <- "maurolepore/tor"
-  expect_equal(attributes(ghr_ls(path))$path, path)
 })
